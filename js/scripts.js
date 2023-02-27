d8751b44c53455eed3c16014d1c650174437acdc
abrmoralesm <abrmoralesm@gmail.com>
2023-02-27 10:41:12 +0100

    subiendo

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+const reciveJoke = async()=>{
+    try{
+        const answer = await fetch("https://icanhazdadjoke.com/", {
+            headers: {
+              Accept: "application/json",
+            },
+          });
+          const chiste = await answer.json();
+          console.log(`Accudit: ${chiste.joke}`)
+
+    }
+    catch(err){
+        console.log(err.message);
+
+    }
+};
