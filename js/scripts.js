94ca80ba878520368411bb097336ea3ba1351d32
abrmoralesm <abrmoralesm@gmail.com>
2023-02-27 15:16:23 +0100

    Exercici 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
-const reciveJoke = async()=>{
-    try{
-        const answer = await fetch("https://icanhazdadjoke.com/", {
-            headers: {
-              Accept: "application/json",
-            },
-          });
-          const chiste = await answer.json();
-          console.log(`Accudit: ${chiste.joke}`);
-          document.querySelector("#textJoke").innerHTML=chiste.joke;
-                    
+let chiste;
+const receive = [];
 
-    }
-    catch(err){
-        console.log(err.message);
-
-    }
+const showButtons = () =>document.querySelectorAll("[buttonScore]").forEach(button => button.style.display = "block");
+   
+const receiveJoke = async () => {
+  try {
+    const answer = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const json = await answer.json();
+    chiste = json.joke;
+    console.log(`Accudit: ${chiste}`);
+    document.querySelector("#textJoke").innerHTML = `" ${chiste} "`;
+  } catch (err) {
+    console.log(err.message);
+  }
+  showButtons();
 };
+
+function score(num) {
+  const report = {
+    joke: chiste,
+    score: num,
+    date: new Date().toISOString(),
+  };
+  receive.push(report);
+  console.table(receive);
+}
