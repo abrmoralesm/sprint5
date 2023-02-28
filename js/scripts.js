fbeebdecb1363e770bbd4f0471c34d13aeacecc4
abrmoralesm <abrmoralesm@gmail.com>
2023-02-28 11:05:19 +0100

    Exercici 5 i 4
    Código del ejercicio 5 y mejoras en el 4.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,13 @@ const showButtons = () =>
     .querySelectorAll("[buttonScore]")
     .forEach((button) => (button.style.display = "block"));
 
+const randomJoke = () => {
+  const random = Math.random();
+  console.log(`Ramdom Joke: ${random}`);
+  if (random < 0.5) receiveJoke();
+  else receiveJoke2();
+};
+
 const receiveJoke = async () => {
   try {
     const answer = await fetch("https://icanhazdadjoke.com/", {
@@ -24,6 +31,19 @@ const receiveJoke = async () => {
   showButtons();
 };
 
+const receiveJoke2 = async () => {
+  try {
+    const answer = await fetch("https://api.chucknorris.io/jokes/random");
+    const json = await answer.json();
+    chiste = json.value;
+    console.log(`Accudit Chuck: ${chiste}`);
+  } catch (err) {
+    console.log(err.message);
+  }
+  document.querySelector("#textJoke").innerHTML = `" ${chiste} "`;
+  showButtons();
+};
+
 function score(num) {
   const report = {
     joke: chiste,
@@ -34,21 +54,30 @@ function score(num) {
   console.table(receive);
 }
 
+const weatherText = document.getElementById("text-weather");
 
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(async function (position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f16094b38276a07cecad67c1e54bf003`);
-    const data = await response.json();
-
-    const weather = data.weather;
-    const locate = data.name;
-    const weatherDescription = weather[0].description;
-    const temperature = Math.round(data.main.temp);
-    const celsiusTemperature = (temperature - 273.15).toFixed(0);
-
-    document.getElementById("text-weather").innerHTML = `Now, at  ${locate}: ${weatherDescription} and ${celsiusTemperature}ºC`;
-  });
+  navigator.geolocation.getCurrentPosition(
+    async function (position) {
+      try {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const response = await fetch(
+          `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f16094b38276a07cecad67c1e54bf003`
+        );
+        const data = await response.json();
+        const weather = data.weather;
+        const locate = data.name;
+        const weatherDescription = weather[0].description;
+        const temperature = Math.round(data.main.temp);
+        const celsiusTemperature = (temperature - 273.15).toFixed(0);
+        weatherText.innerHTML = `Now, at  ${locate}: ${weatherDescription} and ${celsiusTemperature}ºC`;
+      } catch (err) {
+        console.log(err.message);
+      }
+    },
+    function (error) {
+      console.log(error.message);
+    }
+  );
 }
