1f69eba8771ca42eebc3bceb0cda2ba513927253
abrmoralesm <abrmoralesm@gmail.com>
2023-02-28 09:33:22 +0100

    Mejores exercici 4
    Cambio .then por async/await

--- js/scripts.js
+++ js/scripts.js
@@ -34,23 +34,21 @@ function score(num) {
   console.table(receive);
 }
 
+
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (position) {
+  navigator.geolocation.getCurrentPosition(async function (position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f16094b38276a07cecad67c1e54bf003
-    `)
-      .then((response) => response.json())
-      .then((data) => {
-        const weather = data.weather;
-        const locate = data.name;
-        const weatherDescription = weather[0].description;
-        const temperature = Math.round(data.main.temp);
-        const celsiusTemperature = (temperature - 273.15).toFixed(0);
-        document.getElementById(
-          "text-weather"
-        ).innerHTML = `Now, at  ${locate}: ${weatherDescription} and ${celsiusTemperature}ºC`;
-      });
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f16094b38276a07cecad67c1e54bf003`);
+    const data = await response.json();
+
+    const weather = data.weather;
+    const locate = data.name;
+    const weatherDescription = weather[0].description;
+    const temperature = Math.round(data.main.temp);
+    const celsiusTemperature = (temperature - 273.15).toFixed(0);
+
+    document.getElementById("text-weather").innerHTML = `Now, at  ${locate}: ${weatherDescription} and ${celsiusTemperature}ºC`;
   });
 }
